fcd5fa94be4315dd80b96ad40aab606bbc82fbfd
Roger Alencar <rogersalencar@hotmail.com>
2022-09-04 21:50:07 -0300

    Versão 0.1 - Campo e Drag-Drop

--- frontend/futebolAngular/src/app/app.component.ts
+++ frontend/futebolAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'futebolAngular';
+  all = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  even = <number[]>[];
+
+  drop(event: CdkDragDrop<number[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      this.even = [event.item.data];
+    }
+  }
+
+  /** Predicate function that only allows even numbers to be dropped into a list. */
+  evenPredicate(item: CdkDrag<number>) {
+    return item.data % 2 === 0;
+  }
+
+  /** Predicate function that doesn't allow items to be dropped into a list. */
+  noReturnPredicate() {
+    return false;
+  }
 }
