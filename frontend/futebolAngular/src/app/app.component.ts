9a21b3ce5cad0846de274078258f2866b44a675d
Eduardo <dudualvim20@gmail.com>
2022-10-02 16:57:30 -0300

    Arrumando o frontend

--- frontend/futebolAngular/src/app/app.component.ts
+++ frontend/futebolAngular/src/app/app.component.ts
@@ -1,29 +1,80 @@
-import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
-import { CdkDragDrop, moveItemInArray, CdkDrag, CdkDragMove, CdkDragStart, CdkDragEnd } from '@angular/cdk/drag-drop';
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
+import {
+  CdkDragStart,
+  CdkDragMove,
+  CdkDragDrop,
+  moveItemInArray,
+  copyArrayItem,
+  CdkDragEnd,
+  CdkDrag
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  constructor() {}
+
+export class AppComponent implements OnInit {
+  title = 'Angular';
   position = "";
+
+  botaoClicado() {
+    const invalido = this.jogadores.some(jogador => {
+      return jogador.x < 16 || jogador.x > 1900 || jogador.y < 80 || jogador.y > 890;
+    })
+    if(invalido){
+      alert('Todos os jogadores devem estar em campo!');
+    }else{
+      alert('Grafo Feito!');
+    }
+  }
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
 
   panelOpenState = false;
 
-  @ViewChild("dropZone", { read: ElementRef, static: true })
+  @ViewChild('dropZone', {read: ElementRef, static: true})
   dropZone: ElementRef;
 
   _currentIndex;
   _currentField;
   _pointerPosition;
 
-  // Função criada somente para mostrar que os valores de X e Y estão sendo atualizados ao mover  
+  // Função criada somente para mostrar que os valores de X e Y estão sendo atualizados ao mover
   teste(event: CdkDragEnd) {
     let e = event.source.data;
     console.log(e);
   }
+
+  jogadores: { valor: number; nome: string; x: number; y: number }[] = [
+    {valor: 1, nome: 'R10', x: 0, y: 0},
+    {valor: 2, nome: 'R9', x: 0, y: 0},
+    {valor: 3, nome: 'R ', x: 0, y: 0},
+    {valor: 4, nome: 'Rs0', x: 0, y: 0},
+    {valor: 5, nome: 'Rd0', x: 0, y: 0},
+    {valor: 6, nome: 'Ra0', x: 0, y: 0},
+    {valor: 7, nome: 'R1f', x: 0, y: 0},
+    {valor: 8, nome: 'R1f', x: 0, y: 0},
+    {valor: 9, nome: 'R1f', x: 0, y: 0},
+    {valor: 10, nome: 'R1f', x: 0, y: 0},
+    {valor: 11, nome: 'R1f', x: 0, y: 0},
+  ];
+
+  // Atualiza a posição de cada vértice
+  atualizarPosicao(data: any, x: number, y: number) {
+    for (let i in this.jogadores) {
+      if (this.jogadores[i] === data) {
+        this.jogadores[i]["x"] = x;
+        this.jogadores[i]["y"] = y;
+      }
+    }
+  }
+
+  fields: any[] = [];
 
   // Responsável por pegar as coordenadas de cada vértice
   moved(event: CdkDragMove) {
@@ -31,45 +82,29 @@ export class AppComponent {
     this.position = `Posição X: ${this._pointerPosition.x} - Y: ${this._pointerPosition.y}`;
     this.atualizarPosicao(event.source.data, this._pointerPosition.x, this._pointerPosition.y);
   }
-  
-  // No caso, esse array de objetos são os vértices
-  jogadores: {valor: number, nome: string, x: number, y: number}[] = [
-    { "valor": 1, "nome": "R10", "x": 0, "y": 0},
-    { "valor": 2, "nome": "R9", "x": 0, "y": 0},
-    { "valor": 3, "nome": "R ", "x": 0, "y": 0},
-    { "valor": 4, "nome": "Rs0", "x": 0, "y": 0},
-    { "valor": 5, "nome": "Rd0", "x": 0, "y": 0},
-    { "valor": 6, "nome": "Ra0", "x": 0, "y": 0},
-    { "valor": 7, "nome": "R1f", "x": 0, "y": 0},
-  ]
 
-  // Atualiza a posição de cada vértice 
-  atualizarPosicao(data: any, x: number, y: number){
-    for(let i in this.jogadores){
-      if(this.jogadores[i] === data){
-        this.jogadores[i]["x"] = x;
-        this.jogadores[i]["y"] = y;
-      }
-    }
-  }
-
-  even = <any[]>[];
 
   itemDropped(event: CdkDragDrop<any[]>) {
     const rect = event.item.element.nativeElement.getBoundingClientRect();
     const rectDrop = this.dropZone.nativeElement.getBoundingClientRect();
     const top = rect.top + event.distance.y - rectDrop.top;
     const left = rect.left + event.distance.x - rectDrop.left;
-    if (top >= 0 && top <= rectDrop.height - rect.height &&
-        left >= 0 && left <= rectDrop.width - rect.width) {
-      event.item.data.top = top + "px";
-      event.item.data.left = left + "px"; 
-      this.addField({ ...event.item.data }, event.currentIndex);
-      
+    console.log(rect, rectDrop, top, left, event);
+    if (
+      top >= 0 &&
+      top <= rectDrop.height - rect.height &&
+      left >= 0 &&
+      left <= rectDrop.width - rect.width
+    ) {
+      event.item.data.top = top + 'px';
+      event.item.data.left = left + 'px';
+      this.addField({...event.item.data}, event.currentIndex);
     }
   }
 
-  addField(number: number, index: number) {
-    this.even.splice(index, 0, number);
+  addField(fieldType: number, index: number) {
+    this.fields.splice(index, 0, fieldType);
   }
+
 }
+
--- frontend/futebolAngular/src/app/app.module.ts
+++ frontend/futebolAngular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { SharedService } from './shared.service';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { DragDropModule } from '@angular/cdk/drag-drop'
-import {MatButtonModule} from '@angular/material/button'; 
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
